c7ae8907b46c56e523681083ae3d3efa6bbfed64
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2021-08-12 11:02:14 +0300

    fixes upgrade

--- src/Upgrades/Settings.php
+++ src/Upgrades/Settings.php
@@ -2,14 +2,12 @@
 
 namespace LaravelEnso\Facebook\Upgrades;
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use LaravelEnso\Facebook\Models\Settings as Model;
 use LaravelEnso\Upgrade\Contracts\MigratesData;
-use LaravelEnso\Upgrade\Contracts\MigratesPostDataMigration;
 use LaravelEnso\Webshop\Models\Settings as WebshopSettings;
 
-class Settings implements MigratesData, MigratesPostDataMigration
+class Settings implements MigratesData
 {
     public function isMigrated(): bool
     {
@@ -25,14 +23,5 @@ class Settings implements MigratesData, MigratesPostDataMigration
             'page_id' => $settings->facebook_page_id,
             'domain_verification' => (bool) $settings->facebook_domain_verification,
         ]);
-    }
-
-    public function migratePostDataMigration(): void
-    {
-        Schema::table('webshop_settings', function (Blueprint $table) {
-            $table->dropColumn('facebook_verification_code');
-            $table->dropColumn('facebook_page_id');
-            $table->dropColumn('facebook_domain_verification');
-        });
     }
 }
